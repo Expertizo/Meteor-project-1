d75ec7abf91e8be0466dfbcd2cec25a1dca02e08
Ahmed Raza Qadri <razatechuc11@gmail.com>
2017-02-06 22:41:28 +0500

    Site buttons work continue

--- client/views/detail/detail.js
+++ client/views/detail/detail.js
@@ -1,9 +1,21 @@
+Template.details.onCreated(function(){
+  Meteor.subscribe("AllSites");
+});
 Template.details.helpers({
     show: function () {
       var url = Iron.Location.get().path;
-      var id = url.slice(8,26);
+       id = url.slice(8,26);
       console.log("url === Id");
       console.log(id);
       return Sites.find({_id: id}).fetch();    
 }
+});
+Template.details.events({
+    'click .btnContact': function(){
+        var users = Sites.findOne({_id: id});
+        var ids = users.havaClient;
+        alert("btnContact"+users);
+
+     }
+    
 });
